13bb11b89c3d84467744245ce7e35a66bd3d4442
yana1al <alimasharma@gmail.com>
2024-04-16 15:41:13 -0700

    lab is done

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ import os
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Dog, Toy
+from .models import Dog, Toy, Photo
 from .forms import FeedingForm
 
 # Create your views here.
@@ -36,7 +36,7 @@ def remove_toy(request, dog_id, toy_id):
   Dog.objects.get(id=dog_id).toys.remove(toy_id)
   return redirect('detail', dog_id=dog_id)
 
-def add_photo(request, cat_id):
+def add_photo(request, dog_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
         s3 = boto3.client('s3')
@@ -45,7 +45,7 @@ def add_photo(request, cat_id):
             bucket = os.environ['S3_BUCKET']
             s3.upload_fileobj(photo_file, bucket, key)
             url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
-            Photo.objects.create(url=url, cat_id=dog_id)
+            Photo.objects.create(url=url, dog_id=dog_id)
         except Exception as e:
             print('An error occurred uploading file to S3')
             print(e)
